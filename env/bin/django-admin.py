20a1a10db1c00e11eb0853605f581e85e6bb3b96
Bruno de Sá <bcunhasa@gmail.com>
2017-09-15 20:45:26 -0300

    First commit.

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/bruno/Desenvolvimento/Matéria - Desenvolvimento de aplicativos móveis/Projeto/image-editor-server/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
